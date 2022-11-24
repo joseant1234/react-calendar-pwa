1d5165d196b9bc2444664769cb4e1c65331de216
Jose <jose.antonio.umeres@gmail.com>
2022-11-23 20:46:16 -0500

    Added the endpoint to create a new event to the service worker

--- src/sw-template.js
+++ src/sw-template.js
@@ -1,12 +1,15 @@
 // https://developer.chrome.com/docs/workbox/modules/workbox-sw/
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js')
 
+workbox.loadModule('workbox-background-sync');
+
 // durante el proceso de instalación revisa el directorio en cual está e instala todo lo q está en el precache y en workbox.config - globPatterns
 // 'el WB_MANIFEST' es para que el worbox tenga que ir a buscar para leer los archivos que se encuentran ahi
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 const { registerRoute } = workbox.routing;
-const { CacheFirst, NetworkFirst } = workbox.strategies
+const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
 
 registerRoute(
     new RegExp('http://localhost:3001/api/auth/renew'),
@@ -14,7 +17,7 @@ registerRoute(
 )
 
 registerRoute(
-    new RegExp('http://localhost:3001/api/event'),
+    new RegExp('http://localhost:3001/api/events'),
     new NetworkFirst()
 )
 
@@ -30,4 +33,18 @@ registerRoute(
 registerRoute(
     new RegExp('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css'),
     new CacheFirst()
+)
+
+// posteos offline
+// todo lo q no sea metodo GET solo funciona con network only, ya ue varia con la infomacion que lleva, por ejemplo un post lleva un body distinto al de otro request a pesar q es el mismo endpoint
+
+const bgSyncPlugin = new BackgroundSyncPlugin('posteos-offline', {
+    maxRetentionTime: 24 * 60 // máximo guarda 1 día
+})
+registerRoute(
+    new RegExp('http://localhost:3001/api/events'),
+    new NetworkOnly({
+        plugins: [bgSyncPlugin]
+    }),
+    'POST'
 )
