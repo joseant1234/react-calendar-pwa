0c94741004262aa23d46e5ea23695d2ff7479734
Jose <jose.antonio.umeres@gmail.com>
2022-11-24 21:53:57 -0500

    Added a message when the web is offline and online

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { startLogout } from '../../actions/auth';
+import { Offline, Online } from "react-detect-offline";
 
 export const Navbar = () => {
 
@@ -16,8 +17,15 @@ export const Navbar = () => {
             <span className="navbar-brand">
                 { name }
             </span>
-            
-            <button 
+
+            <Online>
+                <span className='text-success'>Online</span>
+            </Online>
+            <Offline>
+                <span className='text-danger'>Offline - Peticiones serán guardadas</span>
+            </Offline>
+
+            <button
                 className="btn btn-outline-danger"
                 onClick={ handleLogout }
             >
